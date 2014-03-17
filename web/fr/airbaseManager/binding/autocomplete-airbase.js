deea2a2a2a12d621d08ce051a44c4db81ce943d4
Tarik Gilani <tarik.gilani@gmail.com>
2014-03-17 15:35:02 +0100

    SERVEUR TEAM REPRESENTE

---
+++ web/fr/airbaseManager/binding/autocomplete-airbase.js
@@ -0,0 +1,24 @@
+define(["jquery", "knockout", "typeahead"], function ($, ko, typeahead) {
+    ko.bindingHandlers.autocompleteAirbase = {
+        init: function (element, valueAccessor, allBindings) {
+            ko.unwrap(valueAccessor());
+
+            var data = new Bloodhound({
+                datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace( d.fullTextSearch ); },
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                local: valueAccessor()
+            });
+
+            data.initialize();
+
+            $(element).typeahead({highlight: true}, {
+                displayKey: "fullTextSearch",
+                source: data.ttAdapter()
+            });
+
+            $(element).on("typeahead:selected", function(e, suggestion, dataset){
+                allBindings().value(suggestion.fullTextSearch);
+            })
+        }
+    };
+});
---
+++ web/fr/airbaseManager/js/facture-viewmodel.js
@@ -0,0 +1,16 @@
+define(["knockout"], function (ko) {
+    return function factureVM() {
+        var self = this;
+
+        //OBSERVABLES
+
+
+        //NOT OBSERVABLES
+
+
+        //SERVICES
+
+        //COMPUTED
+
+    }
+});
