acc758fe5fc894e91946118689149e84db13e070
Tarik Gilani <tarik.gilani@gmail.com>
2014-03-18 14:38:11 +0100

    credit --> Call on pages [Paypal/CB] Work In Progress (30%)

--- web/fr/pilot/js/credit-viewmodel.js
+++ web/fr/pilot/js/credit-viewmodel.js
@@ -9,15 +9,22 @@ define(["knockout" ,"common/js/services-ajax"], function (ko ,services) {
 		 //self.credit=ko.observable();
 		 self.montantCredit=ko.observable(0);
 
-
+		self.callPaypalPage = {
+			paypal: ko.observable(false),
+			checkPaypal: function() {
+				   alert("checked");
+				   return true;
+				}   
+		};
+		ko.applyBindings(callPaypalPage);
         //NOT OBSERVABLES
 
 
         //SERVICES
-		
 		self.crediterCompte = function(){
 			if(self.allValidator()){
-				var newCredit={
+				var newCredit=
+					{
 						pilotAccount_id:self.pilotAccount().id(),
 						price:self.montantCredit(),
 					}
@@ -26,12 +33,11 @@ define(["knockout" ,"common/js/services-ajax"], function (ko ,services) {
 					console.log(data);
 				});
 			}else{
-				alert("tous les champs de sont pas remplis");
+				alert("Les champs n'ont pas tous été saisis");
 			};
 		}
 
         //COMPUTED
-		
 		 self.nouveauCredit=ko.computed(function(){
 		 	var newCredit = parseInt(self.pilotAccount().credit()) + parseInt(self.montantCredit());
 			if(isNaN(newCredit)){
