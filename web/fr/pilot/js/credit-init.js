a473bd13c54c608a874234f31928e4bddacfcf6d
Tarik Gilani <tarik.gilani@gmail.com>
2014-03-20 08:37:50 +0100

    Responsive Modal

--- web/fr/pilot/js/credit-init.js
+++ web/fr/pilot/js/credit-init.js
@@ -169,6 +169,14 @@
 						.trigger('resize')
 						.trigger('scroll');
 				});
+				
+				$(window).resize(function () {
+					fluidDialog();
+				});
+				// catch dialog if opened within a viewport smaller than the dialog width
+				$(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+					fluidDialog();
+				});
 
 		});
 		
@@ -227,8 +235,12 @@
 				success: function(){
 					$( "#dialog_message" ).show();
 					$( "#dialog_message" ).dialog({
-					  modal: true,
-					  width: 620,
+					  width: 'auto', // overcomes width:'auto' and maxWidth bug
+						maxWidth: 600,
+						height: 'auto',
+						modal: true,
+						fluid: true, //new option
+						resizable: false,
 					  buttons: {
 							Ok: function() {
 							  $( this ).dialog( "close" );
@@ -244,4 +256,30 @@
 			}).fail(function(jqXHR){
 				console.log("Error crediterCompte:", jqXHR);
 			});
+		}
+		//--------------------------------------
+		//Fluid Dialog
+		
+		function fluidDialog() {
+			var $visible = $(".ui-dialog:visible");
+			// each open dialog
+			$visible.each(function () {
+				var $this = $(this);
+				var dialog = $this.find(".ui-dialog-content").data("ui-dialog");
+				// if fluid option == true
+				if (dialog.options.fluid) {
+					var wWidth = $(window).width();
+					// check window width against dialog width
+					if (wWidth < (parseInt(dialog.options.maxWidth) + 50))  {
+						// keep dialog from filling entire screen
+						$this.css("max-width", "90%");
+					} else {
+						// fix maxWidth bug
+						$this.css("max-width", dialog.options.maxWidth + "px");
+					}
+					//reposition dialog
+					dialog.option("position", dialog.options.position);
+				}
+			});
+
 		}
